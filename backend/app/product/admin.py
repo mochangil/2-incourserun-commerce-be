14cfabe0901f67f777d2606aee6a65900ae12812
[mochangil] <ahckddlf1234@gmail.com>
2022-06-27 14:32:10 +0900

    Add: Product admin page

--- backend/app/product/admin.py
+++ backend/app/product/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from . import models
-
 # Register your models here.
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = [
+        ('Product information', {'fields': [
+            'productname',
+            'category',
+            'description',
+            'capacity',
+            'price',
+            'photo',
+            'detailphoto',
+        ]})
+    ]
+
+    list_display = (
+        'productname',
+        'category',
+        'description',
+        'capacity',
+        'price',
+        'photo',
+        'detailphoto',
+        'created',
+    )
