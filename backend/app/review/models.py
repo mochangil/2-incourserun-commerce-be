92743f96fa694f812a73b77333f208934b8ead67
[mochangil] <ahckddlf1234@gmail.com>
2022-06-27 10:45:13 +0900

    Add: review's rating validator

--- backend/app/review/models.py
+++ backend/app/review/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from app.product.models import Product
 from app.user.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Review(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,db_column="user")
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
-    rating = models.FloatField(verbose_name="평점")
+    rating = models.FloatField(verbose_name="평점",validators=[MinValueValidator(0.0),MaxValueValidator(5.0)])
     context = models.TextField(verbose_name="후기내용")
     create_date = models.DateTimeField(verbose_name="작성일자",auto_now_add=True)
 
